1a3a08bbb50fbd1045a79abea1b985a37c427eb5
Alma@github.noreply.github.com <almatcss@yahoo.com>
2019-01-26 13:31:07 -0700

    Updates

---
+++ precache-manifest.b7d5ecd55dfc1602432186f5dbde4890.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "71a667b38d13a29e795b",
+    "url": "/Clicky-Game/static/js/main.71a667b3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "71a667b38d13a29e795b",
+    "url": "/Clicky-Game/static/css/main.5e6b70c7.chunk.css"
+  },
+  {
+    "revision": "ab656169ec36b9bc2fae51cb827acd39",
+    "url": "/Clicky-Game/index.html"
+  }
+];
